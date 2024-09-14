b75348dc1f2b8c499ec9234a8ef0db49c7ad5041
ARYAN PURI <aryanpuri600@gmail.com>
2024-09-14 16:51:16 +0530

    good to go

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        appDir: true,
+    },
+    async headers() {
+        return [
+            {
+                source: '/(.*)', // Apply headers to all routes
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0',
+                    },
+                ],
+            },
+        ];
+    },
+};
 
 export default nextConfig;
